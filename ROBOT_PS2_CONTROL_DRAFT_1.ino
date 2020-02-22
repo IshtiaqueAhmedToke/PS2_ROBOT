81be78af293ab435d5ab83ee7c776139e7e29322
Ishtiaque Ahmed <ishtiaqueahmedtoke@gmail.com>
2020-02-22 23:36:26 +0600

    Update ROBOT_PS2_CONTROL_DRAFT_1.ino

--- ROBOT_PS2_CONTROL_DRAFT_1.ino
+++ ROBOT_PS2_CONTROL_DRAFT_1.ino
@@ -9,8 +9,7 @@
   - rumble    = motor rumbling
   uncomment 1 of the lines for each mode selection
 */
-//const int rumble = false;
-//const int pressures = false;
+
 
 PS2X ps2x; //create PS2 Controller object
 int error = 0; 
@@ -93,18 +92,9 @@ void loop() {
   
   if (ps2x.Analog(PSS_LY) == 128) analogWrite(R_PWM_FOR_TWO, 0);
 
-if(ps2x.ButtonPressed(PSB_L1)) 
-    {
-      
-      
-      
-     }
-//if(ps2x.ButtonPressed(PSB_L2)) digitalWrite(8, LOW);//Turn off forward lights
-//if(ps2x.ButtonPressed(PSB_R1)) digitalWrite(A2, HIGH);//Turn on buzzer
-//if(ps2x.ButtonPressed(PSB_R2)) digitalWrite(A2, LOW);//Turn off buzzer
 
-
-  fspeed = map(ps2x.Analog(PSS_LY), 0, 127, 255, 0);
+//if(ps2x.ButtonPressed(PSB_L1)) digitalWrite(23, HIGH);//Turn on forward lights
+//if(ps2x.ButtonPressed(PSB_L2)) digitalWrite(22, LOW);//Turn off forward lights
   
 
 
@@ -136,10 +126,15 @@ if(ps2x.ButtonPressed(PSB_L1))
   
   if (ps2x.Analog(PSS_LX) < 128) { //TURN LEFT
     fspeed = map(ps2x.Analog(PSS_LX), 0, 127, 255, 0);
-   analogWrite(R_PWM_FOR_ONE,0);
-  analogWrite(L_PWM_FOR_ONE,fspeed);
-  analogWrite(R_PWM_FOR_TWO,0);
+   
+analogWrite(R_PWM_FOR_ONE,0);
+  analogWrite(L_PWM_FOR_ONE,0);
+  analogWrite(R_PWM_FOR_TWO,fspeed);
   analogWrite(L_PWM_FOR_TWO,0);
+
+
+
+
    Serial.println("LEFT");
     Serial.println(fspeed);
   }
@@ -149,22 +144,27 @@ if(ps2x.ButtonPressed(PSB_L1))
   if (ps2x.Analog(PSS_LX) > 128) { //TURN RIGHT
     fspeed = map(ps2x.Analog(PSS_LX), 129, 255, 0, 255);
     analogWrite(R_PWM_FOR_ONE,0);
-  analogWrite(L_PWM_FOR_ONE,0);
-  analogWrite(R_PWM_FOR_TWO,fspeed);
+  analogWrite(L_PWM_FOR_ONE,fspeed);
+  analogWrite(R_PWM_FOR_TWO,0);
   analogWrite(L_PWM_FOR_TWO,0);
      Serial.println("RIGHT");
       Serial.println(fspeed);
   }
 
 
+
+
+
+
+
   
   if (ps2x.Analog(PSS_RX) > 128) { //TURN HARD RIGHT
     fspeed = map(ps2x.Analog(PSS_RX), 129, 255, 0, 255);
      
-     analogWrite(R_PWM_FOR_ONE,0);
+      analogWrite(R_PWM_FOR_ONE,0);
   analogWrite(L_PWM_FOR_ONE,fspeed);
-  analogWrite(R_PWM_FOR_TWO,fspeed);
-  analogWrite(L_PWM_FOR_TWO,0);
+  analogWrite(R_PWM_FOR_TWO,0);
+  analogWrite(L_PWM_FOR_TWO,fspeed);
      
      Serial.println("HARD RIGHT");
       Serial.println(fspeed);
@@ -176,8 +176,8 @@ if(ps2x.ButtonPressed(PSB_L1))
   if (ps2x.Analog(PSS_RX) < 128) { //TURN HARD LEFT
     fspeed = map(ps2x.Analog(PSS_RX), 0, 127, 255, 0);
    
-   analogWrite(R_PWM_FOR_ONE,0);
-  analogWrite(L_PWM_FOR_ONE,fspeed);
+    analogWrite(R_PWM_FOR_ONE,fspeed);
+  analogWrite(L_PWM_FOR_ONE,0);
   analogWrite(R_PWM_FOR_TWO,fspeed);
   analogWrite(L_PWM_FOR_TWO,0);
    Serial.println("HARD LEFT");
@@ -186,74 +186,8 @@ if(ps2x.ButtonPressed(PSB_L1))
 
 
 
-  
-  if ((ps2x.Analog(PSS_RY) > 128) && (ps2x.Analog(PSS_RX) < 128)) { //TURN BACK LEFT
-    fspeed = map(ps2x.Analog(PSS_RY), 129, 255, 0, 255);
-            analogWrite(R_PWM_FOR_ONE,(fspeed/2));
-            analogWrite(L_PWM_FOR_ONE,0);
-            analogWrite(R_PWM_FOR_TWO,0);
-            analogWrite(L_PWM_FOR_TWO,fspeed);
-           
-            Serial.println("BACK LEFT");
-            Serial.println(fspeed);
-  
-  }
-
-
-  
-  if ((ps2x.Analog(PSS_RY) > 128) && (ps2x.Analog(PSS_RX) > 128)) { //TURN BACK RIGHT
-    fspeed = map(ps2x.Analog(PSS_RY), 129, 255, 0, 255);
-    
-    analogWrite(R_PWM_FOR_ONE,fspeed);
-    analogWrite(L_PWM_FOR_ONE,0);
-    analogWrite(R_PWM_FOR_TWO,0);
-    analogWrite(L_PWM_FOR_TWO,(fspeed/2));
-    
-    Serial.println("BACK RIGHT");
-     Serial.println(fspeed);
-  } 
-
-   if(ps2x.ButtonPressed(PSB_L1)) 
-    {
-      
-    analogWrite(R_PWM_FOR_ONE,0);
-    analogWrite(L_PWM_FOR_ONE,fspeed);
-    analogWrite(R_PWM_FOR_TWO,fspeed);
-    analogWrite(L_PWM_FOR_TWO,0);
-    Serial.println("HARD LEFT");
-    Serial.println(fspeed);
-      
-    }
-
-    
-    if(ps2x.ButtonPressed(PSB_R1)) 
-    {
-      
-      
-     analogWrite(R_PWM_FOR_ONE,0);
-     analogWrite(L_PWM_FOR_ONE,fspeed);
-     analogWrite(R_PWM_FOR_TWO,fspeed);
-     analogWrite(L_PWM_FOR_TWO,0);
-     
-      Serial.println("HARD RIGHT");
-      Serial.println(fspeed);
-      
-    }
-
-
-
-  
-  if (fspeed < 40) { //STOP 
-    analogWrite(R_PWM_FOR_ONE,0);
-    analogWrite(L_PWM_FOR_ONE,0);
-    analogWrite(R_PWM_FOR_TWO,0);
-    analogWrite(L_PWM_FOR_TWO,0);
-
-    Serial.println("STOP");
-  }
-
-
-  if(ps2x.Button(PSB_PAD_UP)) {
+ 
+ if(ps2x.Button(PSB_PAD_UP)) {
       
     analogWrite(R_PWM_FOR_ONE,0);
     analogWrite(L_PWM_FOR_ONE,255);
@@ -266,8 +200,8 @@ if(ps2x.ButtonPressed(PSB_L1))
       if(ps2x.Button(PSB_PAD_RIGHT)){
 
       analogWrite(R_PWM_FOR_ONE,0);
-      analogWrite(L_PWM_FOR_ONE,0);
-      analogWrite(R_PWM_FOR_TWO,255);
+      analogWrite(L_PWM_FOR_ONE,255);
+      analogWrite(R_PWM_FOR_TWO,0);
       analogWrite(L_PWM_FOR_TWO,0);
       Serial.println("RIGHT");
 
@@ -276,8 +210,8 @@ if(ps2x.ButtonPressed(PSB_L1))
       if(ps2x.Button(PSB_PAD_LEFT)){
        
       analogWrite(R_PWM_FOR_ONE,0);
-      analogWrite(L_PWM_FOR_ONE,255);
-      analogWrite(R_PWM_FOR_TWO,0);
+      analogWrite(L_PWM_FOR_ONE,0);
+      analogWrite(R_PWM_FOR_TWO,255);
       analogWrite(L_PWM_FOR_TWO,0);
       Serial.println("LEFT");
       
@@ -292,14 +226,7 @@ if(ps2x.ButtonPressed(PSB_L1))
           Serial.println("BACK");
           
       }  
-      if (!ps2x.Button(PSB_PAD_DOWN) && !ps2x.Button(PSB_PAD_UP) && !ps2x.Button(PSB_PAD_RIGHT) && !ps2x.Button(PSB_PAD_LEFT)) {
-        analogWrite(L_PWM_FOR_ONE, 0);
-        analogWrite(R_PWM_FOR_ONE, 0);
 
-        analogWrite(L_PWM_FOR_TWO, 0);
-        analogWrite(R_PWM_FOR_TWO, 0);
-        
-      }
 
  
   delay(50);
