3a905b962b16cbb1cd8959cbb48ec61f1835f601
Ishtiaque Ahmed <ishtiaqueahmedtoke@gmail.com>
2020-04-20 23:25:00 +0600

    Update ROBOT_PS2_CONTROL_DRAFT_1.ino

--- ROBOT_PS2_CONTROL_DRAFT_1.ino
+++ ROBOT_PS2_CONTROL_DRAFT_1.ino
@@ -1,4 +1,3 @@
-
 #include <PS2X_lib.h>//include library code
 //************************************************************
 
@@ -12,7 +11,7 @@
 
 
 PS2X ps2x; //create PS2 Controller object
-int error = 0; 
+int error = 0;
 byte type = 0;
 byte vibrate = 0;
 int fspeed;//forward speed
@@ -28,7 +27,7 @@ int L_PWM_FOR_TWO=8;
 int R_PWM_FOR_TWO=9;
 
 void setup() {
- 
+
   //sets all DC motors pins as OUTPUTs
  pinMode(L_EN_FOR_ONE,OUTPUT);
 pinMode(R_EN_FOR_ONE,OUTPUT);
@@ -46,7 +45,7 @@ digitalWrite(R_EN_FOR_TWO,HIGH);
 
 
 Serial.begin(57600);
-  
+
  error = ps2x.config_gamepad(13,11,10,12, true, true);   //setup pins and settings:  GamePad(clock, command, attention, data, Pressures?, Rumble?) check for error
 
  // Check for error
@@ -56,13 +55,13 @@ Serial.begin(57600);
 }
  else if(error == 1)
   Serial.println("No controller found, check wiring or reset the Arduino");
-   
+
  else if(error == 2)
   Serial.println("Controller found but not accepting commands");
-  
+
  else if(error == 3)
   Serial.println("Controller refusing to enter Pressures mode, may not support it.");
-   
+
  // Check for the type of controller
  type = ps2x.readType();
  switch(type) {
@@ -85,68 +84,65 @@ Serial.begin(57600);
 void loop() {
   ps2x.read_gamepad();
   if (ps2x.Analog(PSS_LY) == 128) analogWrite(L_PWM_FOR_ONE, 0);
-  
+
   if (ps2x.Analog(PSS_LY) == 128) analogWrite(R_PWM_FOR_ONE, 0);
-  
+
   if (ps2x.Analog(PSS_LY) == 128) analogWrite(L_PWM_FOR_TWO, 0);
-  
+
   if (ps2x.Analog(PSS_LY) == 128) analogWrite(R_PWM_FOR_TWO, 0);
 
 
 //if(ps2x.ButtonPressed(PSB_L1)) digitalWrite(23, HIGH);//Turn on forward lights
 //if(ps2x.ButtonPressed(PSB_L2)) digitalWrite(22, LOW);//Turn off forward lights
-  
 
 
-  
-  
+
+
+
   if (ps2x.Analog(PSS_LY) >128) { //MOVE BACK
     fspeed = map(ps2x.Analog(PSS_LY), 129, 255, 0, 255);
-  analogWrite(R_PWM_FOR_ONE,fspeed);
-  analogWrite(L_PWM_FOR_ONE,0);
-  analogWrite(R_PWM_FOR_TWO,0);
-  analogWrite(L_PWM_FOR_TWO,fspeed);
-  Serial.println("BACK");
-  Serial.println(fspeed);
+    analogWrite(R_PWM_FOR_ONE,0);
+    analogWrite(L_PWM_FOR_ONE,fspeed);
+    analogWrite(R_PWM_FOR_TWO,fspeed);
+    analogWrite(L_PWM_FOR_TWO,0);
+    Serial.println(fspeed);
+    Serial.println("BACKWARD");
   }
 
 
 
   if (ps2x.Analog(PSS_LY) < 128) { //MOVE FWD
     fspeed = map(ps2x.Analog(PSS_LY), 0, 127, 255, 0);
-    analogWrite(R_PWM_FOR_ONE,0);
-    analogWrite(L_PWM_FOR_ONE,fspeed);
-    analogWrite(R_PWM_FOR_TWO,fspeed);
-    analogWrite(L_PWM_FOR_TWO,0);
+    analogWrite(R_PWM_FOR_ONE,fspeed);
+ analogWrite(L_PWM_FOR_ONE,0);
+ analogWrite(R_PWM_FOR_TWO,0);
+ analogWrite(L_PWM_FOR_TWO,fspeed);
     Serial.println(fspeed);
     Serial.println("FORWARD");
   }
 
 
-  
+
   if (ps2x.Analog(PSS_LX) < 128) { //TURN LEFT
     fspeed = map(ps2x.Analog(PSS_LX), 0, 127, 255, 0);
-   
-analogWrite(R_PWM_FOR_ONE,0);
+
+analogWrite(R_PWM_FOR_ONE,fspeed);
   analogWrite(L_PWM_FOR_ONE,0);
-  analogWrite(R_PWM_FOR_TWO,fspeed);
+  analogWrite(R_PWM_FOR_TWO,0);
   analogWrite(L_PWM_FOR_TWO,0);
-
-
-
-
    Serial.println("LEFT");
     Serial.println(fspeed);
   }
 
 
-  
+
   if (ps2x.Analog(PSS_LX) > 128) { //TURN RIGHT
     fspeed = map(ps2x.Analog(PSS_LX), 129, 255, 0, 255);
+   
     analogWrite(R_PWM_FOR_ONE,0);
-  analogWrite(L_PWM_FOR_ONE,fspeed);
+  analogWrite(L_PWM_FOR_ONE,0);
   analogWrite(R_PWM_FOR_TWO,0);
-  analogWrite(L_PWM_FOR_TWO,0);
+  analogWrite(L_PWM_FOR_TWO,fspeed);
      Serial.println("RIGHT");
       Serial.println(fspeed);
   }
@@ -157,25 +153,25 @@ analogWrite(R_PWM_FOR_ONE,0);
 
 
 
-  
+
   if (ps2x.Analog(PSS_RX) > 128) { //TURN HARD RIGHT
     fspeed = map(ps2x.Analog(PSS_RX), 129, 255, 0, 255);
-     
+
       analogWrite(R_PWM_FOR_ONE,0);
   analogWrite(L_PWM_FOR_ONE,fspeed);
   analogWrite(R_PWM_FOR_TWO,0);
   analogWrite(L_PWM_FOR_TWO,fspeed);
-     
+
      Serial.println("HARD RIGHT");
       Serial.println(fspeed);
   }
 
 
 
-  
+
   if (ps2x.Analog(PSS_RX) < 128) { //TURN HARD LEFT
     fspeed = map(ps2x.Analog(PSS_RX), 0, 127, 255, 0);
-   
+
     analogWrite(R_PWM_FOR_ONE,fspeed);
   analogWrite(L_PWM_FOR_ONE,0);
   analogWrite(R_PWM_FOR_TWO,fspeed);
@@ -186,48 +182,48 @@ analogWrite(R_PWM_FOR_ONE,0);
 
 
 
- 
+
  if(ps2x.Button(PSB_PAD_UP)) {
-      
-    analogWrite(R_PWM_FOR_ONE,0);
-    analogWrite(L_PWM_FOR_ONE,255);
-    analogWrite(R_PWM_FOR_TWO,255);
-    analogWrite(L_PWM_FOR_TWO,0);
-   
+
+    analogWrite(R_PWM_FOR_ONE,255);
+          analogWrite(L_PWM_FOR_ONE,0);
+          analogWrite(R_PWM_FOR_TWO,0);
+          analogWrite(L_PWM_FOR_TWO,255);
+
     Serial.println("FORWARD");
       }
       // TURN RIGHT
       if(ps2x.Button(PSB_PAD_RIGHT)){
 
-      analogWrite(R_PWM_FOR_ONE,0);
-      analogWrite(L_PWM_FOR_ONE,255);
-      analogWrite(R_PWM_FOR_TWO,0);
-      analogWrite(L_PWM_FOR_TWO,0);
+        analogWrite(R_PWM_FOR_ONE,0);
+    analogWrite(L_PWM_FOR_ONE,0);
+    analogWrite(R_PWM_FOR_TWO,0);
+    analogWrite(L_PWM_FOR_TWO,255);
       Serial.println("RIGHT");
 
       }
       // TURN LEFT
       if(ps2x.Button(PSB_PAD_LEFT)){
-       
-      analogWrite(R_PWM_FOR_ONE,0);
-      analogWrite(L_PWM_FOR_ONE,0);
-      analogWrite(R_PWM_FOR_TWO,255);
-      analogWrite(L_PWM_FOR_TWO,0);
+
+      analogWrite(R_PWM_FOR_ONE,255);
+       analogWrite(L_PWM_FOR_ONE,0);
+       analogWrite(R_PWM_FOR_TWO,0);
+       analogWrite(L_PWM_FOR_TWO,0);
       Serial.println("LEFT");
-      
+
       }
       // MOVE BACK
       if(ps2x.Button(PSB_PAD_DOWN)){
-         
-          analogWrite(R_PWM_FOR_ONE,255);
-          analogWrite(L_PWM_FOR_ONE,0);
-          analogWrite(R_PWM_FOR_TWO,0);
-          analogWrite(L_PWM_FOR_TWO,255);
+
+         analogWrite(R_PWM_FOR_ONE,0);
+          analogWrite(L_PWM_FOR_ONE,255);
+          analogWrite(R_PWM_FOR_TWO,255);
+          analogWrite(L_PWM_FOR_TWO,0);
           Serial.println("BACK");
-          
-      }  
+
+      }
 
 
- 
+
   delay(50);
 }
